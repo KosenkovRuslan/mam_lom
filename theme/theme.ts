87e3d870522a5644419e404b0d6bac8bcf12a8a8
Ruslan Kosenkov <rkosenkov@kvant-is.ru>
2024-05-30 18:14:30 +0300

    Check type document

--- theme/theme.ts
+++ theme/theme.ts
@@ -1,4 +1,5 @@
 setInterval( ()=> {
+	if (typeof document === 'undefined') return
 	document.documentElement.setAttribute(
 		'project_lom_theme',
 		new Date().getSeconds() < 30 ? 'light' : 'dark',
